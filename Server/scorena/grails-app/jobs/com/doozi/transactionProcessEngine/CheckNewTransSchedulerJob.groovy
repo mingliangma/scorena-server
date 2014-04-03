77ee977076e9392bf05e2a4f7887324951349e99
mingliangma <mingliang.ma@gmail.com>
2014-04-02 22:21:10 -0400

    fixing build exception

--- Server/scorena/grails-app/jobs/com/doozi/transactionProcessEngine/CheckNewTransSchedulerJob.groovy
+++ Server/scorena/grails-app/jobs/com/doozi/transactionProcessEngine/CheckNewTransSchedulerJob.groovy
@@ -3,12 +3,11 @@ package com.doozi.transactionProcessEngine
 
 
 class CheckNewTransSchedulerJob {
-
-	def newGameResultFetcherService
+    def newGameResultFetcherService
 	static triggers = {
 	  simple name: 'newGameResultTrigger', repeatInterval: 10000 // execute job once in 5 seconds
 	}
-	
+
 	def execute() {		
 		newGameResultFetcherService.printNowTime(new Date())		
 	}
