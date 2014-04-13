0ef68e5756b30eaf7001a4a0af3d821401cc89c7
Heng Kuang <heng.yuan.kuang@gmail.com>
2014-04-13 19:55:58 -0400

    Update the data source for heng environment and fix the bug cannot stop grails application.

--- Server/scorena/grails-app/conf/DataSource.groovy
+++ Server/scorena/grails-app/conf/DataSource.groovy
@@ -1,12 +1,14 @@
 
 
-dataSource {
+dataSource 
+{
     pooled = true
     driverClassName = "com.mysql.jdbc.Driver"
-	dialect = org.hibernate.dialect.MySQL5InnoDBDialect
-    
+	dialect = org.hibernate.dialect.MySQL5InnoDBDialect    
 }
-hibernate {
+
+hibernate 
+{
     cache.use_second_level_cache = true
     cache.use_query_cache = true
     cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
@@ -14,33 +16,45 @@ hibernate {
 }
 
 // environment specific settings
-environments {
-    development {
-        dataSource {
+environments 
+{
+    development 
+	{
+        dataSource 
+		{
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = "jdbc:mysql://localhost/scorena_dev?useUnicode=yes&characterEncoding=UTF-8"
 			username = "root"
 			password = "root"
         }
     }
+	
 	//jdbc:mysql://mydbinstance.cce59dcpxmml.us-west-2.rds.amazonaws.com:3306/scorenaT?user=scorenaadmin&password=scorenaadmin
-    test { 
-        dataSource {
+	
+    test 
+	{ 
+        dataSource 
+		{
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
             url = System.getProperty("JDBC_CONNECTION_STRING")
-//			url = "jdbc:mysql://mydbinstance.cce59dcpxmml.us-west-2.rds.amazonaws.com:3306/scorenaT?useUnicode=yes&characterEncoding=UTF-8"
-//			username = "scorenaadmin"
-//			password = "scorenaadmin"    
+			//url = "jdbc:mysql://mydbinstance.cce59dcpxmml.us-west-2.rds.amazonaws.com:3306/scorenaT?useUnicode=yes&characterEncoding=UTF-8"
+			//username = "scorenaadmin"
+			//password = "scorenaadmin"    
 		}
     }
-    production {
-        dataSource {
+	
+    production 
+	{
+        dataSource 
+		{
             dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
 			url =System.getProperty("JDBC_CONNECTION_STRING")
-//            url = "jdbc:mysql://aa1cnaol294hlvt.cce59dcpxmml.us-west-2.rds.amazonaws.com/ebdb?useUnicode=yes&characterEncoding=UTF-8"
-//			username = "scorenaadmin"
-//			password = "scorenaadmin"    
-            properties {
+			//url = "jdbc:mysql://aa1cnaol294hlvt.cce59dcpxmml.us-west-2.rds.amazonaws.com/ebdb?useUnicode=yes&characterEncoding=UTF-8"
+			//username = "scorenaadmin"
+			//password = "scorenaadmin"    
+			
+            properties 
+			{
                maxActive = -1
                minEvictableIdleTimeMillis=1800000
                timeBetweenEvictionRunsMillis=1800000
@@ -54,12 +68,21 @@ environments {
         }
     }
 	
-	heng {
-		dataSource {
+	heng 
+	{
+		dataSource 
+		{
 			dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+			
+			/*
 			url = "jdbc:mysql://hengmysql.cce59dcpxmml.us-west-2.rds.amazonaws.com/HengDb?useUnicode=yes&characterEncoding=UTF-8"
 			username = "HengMySql"
 			password = "HengMySql"
+			*/
+			
+			url = "jdbc:mysql://54.186.147.78/sportsdb?useUnicode=yes&characterEncoding=UTF-8"
+			username = "scorenaadmin"
+			password = "scorenaadmin"
 		}
 	}
 }
