585234d3a5b37240f56eb062db9577d9c9771f68
mingliangma <mingliang.ma@gmail.com>
2014-05-12 17:12:34 -0400

    added ios 8

---
+++ ios/facebet/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  facebet
+//
+//  Created by Kun on 2013-12-03.
+//  Copyright (c) 2013 Doozi Entertainment Company Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FAppDelegate class]));
+    }
+}
---
+++ ios/facebetTests/facebetTests.m
@@ -0,0 +1,34 @@
+//
+//  facebetTests.m
+//  facebetTests
+//
+//  Created by Kun on 2013-12-03.
+//  Copyright (c) 2013 Doozi Entertainment Company Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface facebetTests : XCTestCase
+
+@end
+
+@implementation facebetTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
