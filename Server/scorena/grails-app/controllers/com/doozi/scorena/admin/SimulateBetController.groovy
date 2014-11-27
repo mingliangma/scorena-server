7ccbab6cd683e74fe659ca1dcd43cc7576de9d19
mingliangma <mingliangma@192.168.1.106>
2014-11-27 16:20:07 -0500

    simulate comment
    Signed-off-by: mingliangma <mingliangma@192.168.1.106>

--- Server/scorena/grails-app/controllers/com/doozi/scorena/admin/SimulateBetController.groovy
+++ Server/scorena/grails-app/controllers/com/doozi/scorena/admin/SimulateBetController.groovy
@@ -2,7 +2,12 @@ package com.doozi.scorena.admin
 
 class SimulateBetController {
 	def simulateBetService
+	def simulateCommentService
     def simulateBet() { 		
 		simulateBetService.simulateBetUpcoming()
+	}
+	
+	def simulateComment(){
+		simulateCommentService.simulateComment()
 	}
 }
