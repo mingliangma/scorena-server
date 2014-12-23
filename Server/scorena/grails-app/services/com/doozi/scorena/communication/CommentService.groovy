72ba388529cd2e28531168272aa1241ff42056d3
Thomas Huang <hdj.naruto@gmail.com>
2014-12-22 19:29:11 -0500

    Change logging system settings in Config.groovy. Demo logging system of CommentService.

--- Server/scorena/grails-app/services/com/doozi/scorena/communication/CommentService.groovy
+++ Server/scorena/grails-app/services/com/doozi/scorena/communication/CommentService.groovy
@@ -1,10 +1,12 @@
 package com.doozi.scorena.communication
 
 import org.springframework.transaction.annotation.Transactional
+
 import java.util.List
 
 import com.doozi.scorena.Question
 import com.doozi.scorena.Account
+import com.mysql.jdbc.log.Log;
 
 import grails.plugins.rest.client.RestBuilder
 
@@ -19,52 +21,62 @@ class CommentService {
 
 	def helperService
 	def parseService
-	
+
 	/**
 	 * @brief get comments information of the question
 	 * @param qId:ID of question
 	 * @return List of comments:[body,userId,userName,timeCreated]
 	 */
-    List getExistingComments(qId)
-	{
+	List getExistingComments(qId){
+
+		log.info "getExistingComments(): begins with qId = ${qId}"
+
 		List commentsList=[]
 		Question q = Question.findById(qId)
-		
+		def message
+
 		if (q==null){
-			return [message: "invalid question ID"]
+			message = "invalid question ID"
+			log.error "${message}"
+			return [tips:message]
 		}
-		
+
 		q.comments.each{
 			Account user= Account.findById(it.posterId);
 			if(user == null){
-				return [message:"invalid user ID"]
+				message = "invalid user ID"
+				log.error "${message}"
+				return [tips:message]
 			}
-			
+
 			def userId = user.userId
 			def username = user.username
 			def dateCreated=it.dateCreated
 			dateCreated = helperService.getOutputDateFormat(dateCreated)
-			
+
 			def rest = new RestBuilder()
 			def resp = parseService.retrieveUser(rest, userId)
 			if (resp.status != 200) {
-				println "get user profile failed! (CommentService::getExistingComments)"
-				return [message: "get user profile failed!"]
+				message = "get user profile failed!"
+				log.error "${message}"
+				return [tips:message]
 			}
 			def userProfile = resp.json
-			
+
 			if (userProfile.display_name != null && userProfile.display_name != "") {
 				username = userProfile.display_name
 			}
 			def pictureURL = userProfile.pictureURL
-			
+
 			def comments=[body:it.body, userId:userId, name:username, timeCreated:dateCreated, pictureURL:pictureURL]
 			commentsList.add(comments)
 		}
-		
+
+		log.info "getExistingComments(): ends with commentsList = ${commentsList}"
+
 		return commentsList
 	}
-	
+
 	/**
 	 * @brief add comments to question
 	 * @param userId:userId of the account commenting
@@ -74,32 +86,45 @@ class CommentService {
 	 */
 	@Transactional
 	List writeComments(userId,message,qId){
+
+		log.info "writeComments(): begins with userId = ${userId}, qId = ${qId}, message = ${message}"
+
 		List commentsList=[]
 		Question q = Question.findById(qId)
 		Account user= Account.findByUserId(userId)
 		boolean isValidUserId = false
-		
+		def errorMessage
+
 		if (q==null){
-			return [tips: "invalid question ID"]
+			errorMessage = "invalid question ID"
+			log.error "${errorMessage}"
+			return [tips: errorMessage]
 		}
-		
+
 		if (user==null){
-			return [tips: "invalid user ID"]
+			errorMessage = "invalid user ID"
+			log.error "${errorMessage}"
+			return [tips: errorMessage]
 		}
-		
+
 		if (message==null){
-			return [tips: "null comment"]
+			errorMessage = "null comment"
+			log.error "${errorMessage}"
+			return [tips: errorMessage]
 		}
-		
+
 		if (user != null){
 			isValidUserId = true
 		}
-		
+
 		if(isValidUserId){
 			q.addComment(user, message)
 		}
-		
+
 		commentsList = getExistingComments(qId)
-		return commentsList	
+
+		log.info "writeComments(): ends with commentsList = ${commentsList}"
+
+		return commentsList
 	}
 }
